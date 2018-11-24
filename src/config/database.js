883e878c253f35effa05f2b0be765f97adf2e159
LucasArusiewicz <lucasarusiewicz@gmail.com>
2018-11-23 23:34:25 -0200

    init

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'mysql',
+  host: 'localhost',
+  username: 'root',
+  password: '1234',
+  database: 'cabeleireiro',
+  operatorAliases: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+}
